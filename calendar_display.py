6394b7de70f2c05fbff28ff188239dba5619c97b
SophieRay02 <ray.sop@northeastern.edu>
2022-11-13 14:52:56 -0500

    created new class for calendar display

---
+++ calendar_display.py
@@ -0,0 +1,9 @@
+class CalendarDisplay:
+
+    def calendar_display(self, setfirstweekday(Sunday)):
+        '''This function represents the general calendar-related code for presentation
+        I want Sunday to be the first day presented in the week... need to switch from Monday to Sunday
+        The following methods will provide different ways of displaying the calendar: weekly, monthly, yearly, etc. 
+        '''
+
+        
