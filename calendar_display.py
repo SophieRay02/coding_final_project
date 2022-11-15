5b18ce381906c020c2338d548e31f5ae51e5fa17
SophieRay02 <ray.sop@northeastern.edu>
2022-11-14 22:55:47 -0500

    more future use functions

--- calendar_display.py
+++ calendar_display.py
@@ -12,36 +12,52 @@ class Display:
                 4:'April', 5:'May', 6:'June', 7:'July', 
                 8:'August', 9:'Septemeber', 10:'October',
                 11:'November', 12:'December'}
-        no_leap_year = [31,28,31,30,31,30,31,31,30,31,30,31]
-        leap_year = [31,29,31,30,31,30,31,31,30,31,30,31]
-        date = 0                                                #the choosen date is =0 in order to differentiate between the days in a leap year versus a non-leap year
 
-        if input_year % 4 == 0:
-            for i in range(month-1):
-                date += leap_year[i]
-            
-        else:
-            for i in range(month-1):
-                date += no_leap_year[i]
+no_leap_year = [0, 31,28,31,30,31,30,31,31,30,31,30,31]
+leap_year = [0, 31,29,31,30,31,30,31,31,30,31,30,31]    #must include 0 in beginning bc python is funky like dat
+        # date = 0                                                #the choosen date is =0 in order to differentiate between the days in a leap year versus a non-leap year
 
-        day += date % 7
-        day = day % 7
+        # if input_year % 4 == 0:
+        #     for i in range(month-1):
+        #         date += leap_year[i]
 
-        print(month[input_month], input_year)
-        print('Sun', 'Mon', 'Tue', 'wed', 'Thu', 'Fri', 'Sat')
+        # else:
+        #     for i in range(month-1):
+        #         date += no_leap_year[i]
+
+
+
+
+
+
 
 
 
 ####### CREATING FUTURE USE FUNCTIONS (not sure where to put them yet) #########
+
+# maybe this will set Sunday as weekday(1)??
+(SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY) = range(7)
+
+
 def isleap(year):
     # Returns True for leap years and False for non-leap years
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 ==0)
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 def weekday(year, month, day):
-    return datetime.adte(year, month, day).weekday()
+    # datetime makes Monday the first day on a calendar... I'd like it to be Sunday... not sure how to change that
+    return datetime.date(year, month, day).weekday()
 
+def month_length(year, month):
+    day1 = weekday(year, month, 1)
+    ndays = no_leap_year[month] +(month == 'February' and isleap(year))
+    return day1, ndays
 
+def previous_month(year, month):
+    if month == 1:
+        return year-1, 12       #in the case that the user wants to go back one year but is in January, have to go back to previous year in December
+    else:
+        return year, month-1
 
-
+def 
 
     
